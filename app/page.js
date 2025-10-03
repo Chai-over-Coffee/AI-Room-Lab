cc4fd877c07190b0a84bfbbe2cf9b1cc1f18980b
Ayush Ranjan <chelsea.rnc100@gmail.com>
2025-10-03 16:41:03 +0530

    Added Layout and Multi Models of AI

--- app/page.js
+++ app/page.js
@@ -1,16 +1,12 @@
 "use client";
-import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
-import Image from "next/image";
+import ChatInputBox from "./_components/ChatInputBox";
 
 export default function Home() {
   const {setTheme} = useTheme();
   return (
     <div>
-      <h2>Ayush!</h2>
-      <Button>Click Me!</Button>
-      <Button onClick={() => setTheme('dark')}>Dark</Button>
-      <Button onClick={() => setTheme('light')}>Light</Button>
+      <ChatInputBox />
     </div>
   );
 }
